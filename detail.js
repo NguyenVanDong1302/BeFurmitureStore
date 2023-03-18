855a593aa234988b086c55cf692c1dae68a64830
NguyenVanDong1302 <dongyh2002bghs@gmail.com>
2023-03-19 02:48:25 +0700

    done 99%

---
+++ detail.js
@@ -0,0 +1,25 @@
+// const detailRight = document.querySelector(".content-item__right");
+// const detailLeft = document.querySelector(".content-item__left");
+
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= 40) {
+//     detailRight.classList.add("stickyDetail");
+//   } else {
+//     detailRight.classList.remove("stickyDetail");
+//   }
+//   if (
+//     window.scrollY >=
+//     detailLeft.offsetHeight + detailLeft.offsetTop - screen.availHeight
+//   ) {
+//     console.log("hello");
+//     detailRight.classList.remove("stickyDetail");
+//     detailRight.classList.add("stickyDetail-2");
+//   } else {
+//     detailRight.classList.remove("stickyDetail-2");
+//     detailRight.classList.add("stickyDetail");
+//   }
+// });
+
+// // console.log(detailLeft.offsetHeight);
+// console.log(screen.height);
+// // console.log(detailLeft.offsetTop);
